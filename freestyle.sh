edcada727e9cdf1f11378eeb4994df87c7f804bc
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-04-15 14:57:32 +0100

    shell script update added

--- freestyle.sh
+++ freestyle.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# change to project directory
-cd /path/to/project
-
-# install dependencies
-npm install
-
-# run development server
-npm run start
-
-# build for production
-npm run build
-
-# run tests
-npm run test
+npm --version
