79adcea12806b788585cf9ff831d77807f820e8b
Onumoh Abubakar <abubakaronumoh@gmail.com>
2023-04-15 15:05:31 +0100

    shell script update added con

--- freestyle.sh
+++ freestyle.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# change to project directory
+cd /Downloads/aws-form-event/src
+
+# install dependencies
+npm install
+
+# run development server
+npm run start
+
 # build for production
 npm run build
 
